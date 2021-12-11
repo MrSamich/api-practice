1c2136ee45558e24c9686f60c0c0e4717f98ad98
MrSamich <alexanderjsanchez89@gmail.com>
2021-12-11 08:59:34 -0500

    add fetch

--- api-practice/main.js
+++ api-practice/main.js
@@ -1,6 +1,9 @@
-let text = document.getElementsByClassName('.temp');
+let temperatureEl = document.getElementsByClassName('temperature')[0];
+console.log({temperatureEl})
 
 fetch('http://api.openweathermap.org/data/2.5/forecast?id=5110253&units=imperial&appid=83443629bc34a85a47eb30b373cf91ac')
 .then(result => result.json())
-.then(list => console.log(list.list[0].main.temp))
- 
+.then(json => temperatureEl.append(json.list[0].main.temp))
+
+
+console.log(temperatureEl)
